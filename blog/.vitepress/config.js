85b98c9439a3c49eb0dfe082bbb4acd5f04b2686
fbraem <franky.braem@gmail.com>
2022-01-27 17:41:58 +0100

    Try to use themeConfig...

--- blog/.vitepress/config.js
+++ blog/.vitepress/config.js
@@ -1,9 +1,19 @@
-module.exports = {
+import { defineConfig } from "vitepress";
+
+export default defineConfig({
     title: 'Zumuta!',
     description: "That's the way to do IT",
     markdown: {
         anchor: {
             permalink: false
         }
+    },
+    themeConfig: {
+        repo: 'fbraem/zumuta',
+        docsDir: 'blog',
+        docsBranch: 'master',
+        editLinks: true,
+        editLinkText: 'Edit this page on Github',
+        lastUpdated: 'Last Updated'
     }
-}
+})
