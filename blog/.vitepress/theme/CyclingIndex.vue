2f235a3ea13b589d537b36c59c7eec97b3339f4f
fbraem <franky.braem@gmail.com>
2022-01-28 19:26:46 +0100

    Add hero to CyclingIndex.vue

--- blog/.vitepress/theme/CyclingIndex.vue
+++ blog/.vitepress/theme/CyclingIndex.vue
@@ -5,31 +5,28 @@ import Excerpt from "./Excerpt.vue"
 
 <template>
   <div class="container mx-auto max-w-7xl">
-    <div class="container mx-auto max-w-7xl py-12">
-      <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div class="lg:text-center">
-          <h1 class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-            {{ $frontmatter.title }}
-          </h1>
-          <h3 class="text-base font-semibold tracking-wide">
-            {{ $frontmatter.subtitle }}
-          </h3>
-          <p class="mt-4 max-w-2xl md:text-lg text-gray-500 lg:mx-auto text-base">
-            {{ $frontmatter.description }}
-          </p>
-        </div>
-      </div>
+    <div
+        v-if="$frontmatter.hero"
+        class="my-6"
+    >
+      <img :src="$frontmatter.hero" />
+    </div>
+    <div class="lg:text-center">
+      <h1 class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+        {{ $frontmatter.title }}
+      </h1>
+      <h3 class="text-base font-semibold tracking-wide">
+        {{ $frontmatter.subtitle }}
+      </h3>
+      <p class="mt-4 max-w-2xl md:text-lg text-gray-500 lg:mx-auto text-base">
+        {{ $frontmatter.description }}
+      </p>
     </div>
     <div class="divide-y divide-gray-200">
       <Content />
       <ul class="divide-y divide-gray-200">
         <li class="py-12" v-for="{ title, href, year, excerpt } of posts">
-          <article
-              class="
-            space-y-2
-            xl:grid xl:grid-cols-4 xl:space-y-0 xl:items-baseline
-          "
-          >
+          <article class="space-y-2">
             <Excerpt
                 :href="href"
                 :title="title"
